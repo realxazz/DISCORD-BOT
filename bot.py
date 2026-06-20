d992e7a746556a1c7f5a1061abefee8c12a687ce
realxazz <realxazku@gmail.com>
2026-06-20 14:21:46 +0200

    Add RBXS ticket handling message to bot

--- bot.py
+++ bot.py
@@ -129,6 +129,16 @@ async def on_guild_channel_create(channel):
             "Blade Ball Tickets are manually handled, wait for Grave or an admin."
         )
 
+# =====================================================
+# RBXS SYSTEM
+# =====================================================
+elif cat == "rbxs":
+
+    await asyncio.sleep(1.5)
+
+    await channel.send(
+        "RBXS Tickets are manually handled, wait for Grave or an admin."
+    )
 
 # =========================
 # MESSAGE HANDLER
